92b8e3d070768e724af4f520a4e28b257ce5dd96
Jeff James <jeffjames88@gmail.com>
2015-09-14 15:50:36 -0400

    Update package.js

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'An incredibly easy user-presence package based on konecty:user-presence',
   // URL to the Git repository containing the source code for this package.
-  git: 'https://github.com/jwjames/easy-presence',
+  git: 'https://github.com/jwjames/easy-presence.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
